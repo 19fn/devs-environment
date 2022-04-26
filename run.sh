b6d5476a0ad58bd653a2d36af98db89b7755a898
19fn <fcabrera.all@gmail.com>
2022-04-26 11:00:03 -0300

    Deleted .env artifact scr --Modified docker-compose.yml run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Vars
-CONTAINER=""
+CONTAINER="local-environment"
 ENV_FILE=".env"
-JAR_FILE=$1
+JAR_FILE=$2
 DOCKER_COMPOSE_FILE="docker-compose.yml"
-DC_UP="docker-compose up -d"
+DC_UP="docker-compose up --detach"
+DC_DOWN="docker-compose down"
 
 # Errors
 E_ENV_FILE="[!] Missing file '.env' ..."
@@ -13,21 +14,36 @@ E_JAR_FILE="[!] Cannot found '${JAR_FILE}' ..."
 E_EXT_JAR_FILE="[!] You must provide a '.jar' file ..."
 E_DOCKER_COMPOSE="[!] Missing file 'docker-compose.yml' ..."
 
-getHelp()
+usage()
 {
 	echo
-	echo -e "\n[!] Usage: $0 [path/to/file.jar]\n\n\n\tArgument:\tDescription:\n\tfile.jar\tlaunch file.jar server\n"
+	echo -e "[!] Usage: $0 [--start] || [--stop]\n\n\n\tCMD:\t\tDescription:\n\t--start\t\tdocker compose up\n\t--stop\t\tdocker compose down"
 	echo
 	exit 1
 }
 
-if [ ! -f ${ENV_FILE} ]; then
-	echo
-	echo ${E_ENV_FILE}
-	echo
-	exit 1
-fi
+#dc_start()
+#{
+#	if [ "$(echo ${JAR_FILE} | tr '.' ' ' | awk '{print $NF}')" == "jar" ]; then
+#                sed -i '/ARTIFACT/d' ${ENV_FILE} && \
+#			echo "ARTIFACT=${JAR_FILE}" >> ${ENV_FILE} && \
+#			${DC_UP}
+#        else
+#                echo
+#                echo ${E_EXT_JAR_FILE}
+#                echo
+#        fi
+#}
 
+# .env file
+#if [ ! -f ${ENV_FILE} ]; then
+#	echo
+#	echo ${E_ENV_FILE}
+#	echo
+#	exit 1
+#fi
+
+# docker-compose.yml file
 if [ ! -f ${DOCKER_COMPOSE_FILE} ]; then
 	echo 
 	echo ${E_DOCKER_COMPOSE}
@@ -35,21 +51,16 @@ if [ ! -f ${DOCKER_COMPOSE_FILE} ]; then
 	exit 1
 fi
 
-if [ ! -f ${JAR_FILE} ]; then
-	echo
-	echo ${E_JAR_FILE}
-	echo
-	exit 1
-fi
+# .jar file
+#if [ ! -f ${JAR_FILE} ]; then
+#	echo
+#	echo ${E_JAR_FILE}
+#	echo
+#	exit 1
+#fi
 
-if [ $# -ne 1 ]; then
-	getHelp
-else
-	if [ "$(echo ${JAR_FILE} | tr '.' ' ' | awk '{print $NF}')" == "jar" ]; then
-		${DC_UP}
-       	else
-	       echo
-	       echo ${E_EXT_JAR_FILE}
-	       echo
-	fi	 
-fi
+case $1 in
+	--start) ${DC_UP} ;;
+	--stop) ${DC_DOWN} ;;
+	*) usage ;;
+esac		
