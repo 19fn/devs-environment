1e728e2ed27453f05a7148c7309885b7a2355a7a
19fn <fcabrera.all@gmail.com>
2022-04-26 23:39:36 -0300

    Fix control.sh

--- control.sh
+++ control.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Vars
+arg=$1
 DOCKER_COMPOSE_FILE="docker-compose.yml"
 DC_UP="docker-compose up --detach"
 DC_DOWN="docker-compose down"
@@ -12,7 +13,7 @@ E_TARGET_NOT_FOUND="[!] Directory: 'target' not found ..."
 usage()
 {
 	echo
-	echo -e "[!] Usage: $0 [start] || [stop]\n\n\n\tCommand:\tDescription:\n\tstart\t\tStart local environment container\n\tstop\t\tStop local environment container"
+	echo -e "[!] Usage: $0 [start] || [stop]\n\n\n\tCommand:\tDescription:\n\tstart\t\tStart local environment container\n\tstop\t\tShutdown local environment container"
 	echo
 	exit 1
 }
@@ -20,7 +21,6 @@ usage()
 # target directory
 if ! [[ $(find ../ -maxdepth 1 -type d -name "target") ]]; then
 	echo; echo ${E_TARGET_NOT_FOUND}; echo
-	exit 1
 fi
 
 # docker-compose.yml file
@@ -30,8 +30,8 @@ if [ ! -f ${DOCKER_COMPOSE_FILE} ]; then
 fi
 
 # Main
-case $1 in
+case ${arg} in
 	start) ${DC_UP} ;;
 	stop) ${DC_DOWN} ;;
 	*) usage ;;
-esac		
+esac	
