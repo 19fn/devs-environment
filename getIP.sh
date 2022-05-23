8fbed939ca42fecb2515ca9016bddc11b0d88f32
19fn <fcabrera.all@gmail.com>
2022-05-23 13:37:40 -0300

    Add getIP script

--- getIP.sh
+++ getIP.sh
@@ -5,9 +5,9 @@ getIP()
 	ens33=$(ip a | awk '/ens33/' | grep "inet" | cut -d"/" -f1 | xargs | awk '{print $2}')
 	eth0=$(ip a | awk '/eth0/' | grep "inet" | cut -d"/" -f1 | xargs | awk '{print $2}')
 	
-	if [[ ${ens33} =~ ^[0-9.:]+$ ]]; then
+	if [[ ${ens33} =~ ^[0-9.]+$ ]]; then
 		echo;echo "[+] IP: ${ens33}";echo
-	elif [[ ${eth0} =~ ^[0-9.:]+$ ]]; then
+	elif [[ ${eth0} =~ ^[0-9.]+$ ]]; then
 		echo;echo "[+] IP: ${eth0}";echo
 	else
 		echo;echo "[!] IP can not be found.";echo
